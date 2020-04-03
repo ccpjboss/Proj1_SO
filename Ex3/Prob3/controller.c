fcdd83041dde184b8895bbf8c70deab1b16b261f
ccpjboss <jpccastilho1999@gmail.com>
2020-04-03 20:47:14 +0100

    Controller read agent PID

--- Ex3/Prob3/controller.c
+++ Ex3/Prob3/controller.c
@@ -14,3 +14,31 @@
  * ! SIGTERM also terminates the controller
  * ! If the user presses Crtl+C the controller should send a SIGTERM to agent and exits
  */
+
+#include <signal.h>    /*Signals*/
+#include <sys/types.h> /*Process*/
+#include <unistd.h>    /*Process*/
+#include <stdlib.h>
+#include <stdio.h> /*Files*/
+
+int main(int argc, char const *argv[])
+{
+    pid_t pid;
+    FILE *agentptr;
+
+    if ((agentptr = fopen("agent.pid", "r")) == NULL) /*Open in writing mode*/
+    {
+        printf("File could not be oppened!\n");
+        perror("AGENT.PID\n");
+        exit(1);
+    }
+    else
+    {
+        
+        fscanf(agentptr, "%d", &pid);
+        printf("PID: %d\n",pid);
+        fclose(agentptr); /* fclose closes the file */
+    }
+
+    return 0;
+}
